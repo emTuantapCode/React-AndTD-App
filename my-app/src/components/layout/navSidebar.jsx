9b9f25976c9031f38dbe64173da220984c92932c
tuấn anh đinh <tuananhd.020302@gmail.com>
2022-07-10 21:24:19 +0700

    Thi cong sider

--- my-app/src/components/layout/navSidebar.jsx
+++ my-app/src/components/layout/navSidebar.jsx
@@ -1,102 +1,65 @@
 import { Col, Row } from "antd";
 import logo from '../../asset/logo-icon.png'
 import userIcon from '../../asset/icon/Group 10.png'
-import { Dropdown, Menu, Space } from 'antd';
 import { homeI8 } from "../../i8/homei8";
+import React from 'react';
+import { Layout, Menu } from "antd";
 
-const dropdownMenu = {
-     menuLaptop : (
-        <Menu
-          items={[
-            {
-              label: <a href="#">Laptop MSI</a>,
-              key: '0',
-            },
-            {
-                type: 'divider',
-            },
-            {
-              label: <a href="#">Laptop Asus</a>,
-              key: '1',
-            },
-            {
-              type: 'divider',
-            },
-            {
-              label: <a href="#">Laptop Dell</a>,
-              key: '2',
-            },
-          ]}
-        />
-      ),
-      menuPhone : (
-        <Menu
-          items={[
-            {
-              label: <a href="#">Iphone</a>,
-              key: '0',
-            },
-            {
-                type: 'divider',
-            },
-            {
-              label: <a href="#">Samsung</a>,
-              key: '1',
-            },
-            {
-              type: 'divider',
-            },
-            {
-              label: <a href="#">Xiaomi</a>,
-              key: '2',
-            },
-            {
-                type: 'divider',
-              },
-              {
-                label: <a href="#">Huawei</a>,
-                key: '3',
-              },
-          ]}
-        />
-      )
+const { Sider } = Layout;
+const itemslist = [homeI8.laptop,homeI8.phone,homeI8.keybroad,homeI8.others].map(
+  (value, index) => {
+    const key = String(index + 1);
+    return {
+      key: `sub${key}`,
+      label: `${value}`,
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  }
+);
 
-        }
-function Sider() {
-    return (
-        <>
-            <Row>
-                <Col span={6}
-                    style={{
-                        backgroundColor: '#211D1E',
-                        minWidth: '300px',
-                        minHeight: '1000px',
-                    }}>
-                    <div className="logo">
-                        <img src={logo} alt="logo" />
-                    </div>
-                    <div className="containerMenu">
-                        <div className="userWellcome">
-                            <Row>
-                                <Col><img src={userIcon} alt="user" /></Col>
-                                <Col>{homeI8.wellcom} Username</Col>
-                            </Row>
-                        </div>
-                        <div className="siderMenu">
-                        <Menu
-      onClick={onClick}
-      style={{ width: 256 }}
-      defaultSelectedKeys={['1']}
-      defaultOpenKeys={['sub1']}
-      mode="inline"
-      items={items}
-    />
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-        </>
-    );
+function NavSider() {
+  return (
+    <>
+      <Row>
+        <Col span={6}
+          style={{
+            backgroundColor: '#211D1E',
+            minWidth: '300px',
+            minHeight: '1000px',
+          }}>
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="containerMenu">
+            <div className="userWellcome">
+              <Row>
+                <Col><img src={userIcon} alt="user" /></Col>
+                <Col style={{color:'#fff'}}>{homeI8.wellcom} Username</Col>
+              </Row>
+            </div>
+            <div className="siderMenu">
+              <Sider width="100%" className="site-layout-background">
+                <Menu
+                  mode="inline"
+                  style={{
+                    height: "100%",
+                    borderRight: 0,
+                  }}
+                  items={itemslist}
+                />
+              </Sider>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default Sider;
+export default NavSider;
