d586178f754e9a1dea4e5ae91bebaa8b148a32a4
iamfir3 <88335051+iamfir3@users.noreply.github.com>
2022-07-12 00:58:00 +0700

    update 3

--- my-app/src/components/InputComponent/input.jsx
+++ my-app/src/components/InputComponent/input.jsx
@@ -1,24 +1,10 @@
 import { useState } from "react";
+import "../../sass/inputComponent.scss";
 
 const Input=(props)=>{
-    const [isFocus,setIsFocus]=useState(false);
-    
-    let styles=isFocus?{
-        width: 300+'px',
-        backgroundColor:'#fff',
-        border:'none',
-        outline:'none',
-        borderRadius:24+'px',
-        }:{
-            outline:'none',
-            width: 300+'px',
-            backgroundColor:'#d9d9d9',
-            borderRadius:24+'px',
-            border:'none',
-        }
     return (
         <>
-            <input type={props.type} min={props.min} max={props.max} style={styles} onFocus={()=>setIsFocus(true)} onBlur={()=>setIsFocus(false)}  />
+            <input type={props.type} min={props.min} max={props.max} className='input' />
         </>
     )
 }
--- my-app/src/pages/account/Signup/SignUp.jsx
+++ my-app/src/pages/account/Signup/SignUp.jsx
@@ -1,9 +1,30 @@
 import Input from "../../../components/InputComponent/input"
+import "../../../sass/signin.scss"
+import logo from "../../../asset/logo.svg";
+import {BtnExtra} from "../../../components/btncomponent/btncomponent"
+import "../../../sass/font.scss"
+
 function SignUp() {
     return (
-      <>
-        <Input type='password' min="" max=""></Input>
-      </>
+      <div className="signin-container" >
+        <div style={{display:'block'}}>
+          <img src={logo} style={{paddingBottom:50+'px'}} />
+          <p className='signin-text__bold'>Số điện thoại/Email:</p>
+          <Input type='password' min="" max=""></Input>
+          <p className='signin-text__bold'>Mật khẩu:</p>
+          <Input type='password' min="" max=""></Input>
+          <BtnExtra content="Đăng nhập" style=
+          {{display:'flex',
+          justifyContent:'flex-end'
+        }}></BtnExtra>
+          <p className='signin-text__light'>Bạn chưa có tài khoản?</p>
+          <BtnExtra content="Đăng kí" style={{
+            display:'flex',
+            justifyContent:'flex-end'
+        }}></BtnExtra>
+
+        </div>
+      </div>
     );
   }
   
