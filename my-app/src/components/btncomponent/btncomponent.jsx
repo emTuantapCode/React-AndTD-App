ad1833992ae4a11d0ff8c71854335e7d1287ef32
tuấn anh đinh <tuananhd.020302@gmail.com>
2022-07-15 22:15:32 +0700

    ver2.2

--- my-app/src/components/btncomponent/btncomponent.jsx
+++ my-app/src/components/btncomponent/btncomponent.jsx
@@ -1,23 +1,23 @@
 
-// export const BtnLagre = ({ btnName })=>{
-//     return(
-//         <>
-//         <button className="btnLarge"
-//             style={{
-//                 width: 200,
-//                 height: 80,
-//                 backgroundColor:'#070707',
-//                 color:'#fff',
-//                 margin:'10px 2px 60px 10px',
-//                 textAlign: 'center',
-//             }}
-//         >
-//             <p
-//                 style={{
-//                     fontSize: 20,
-//                     padding:'25px 0'}}
-//             >{btnName}</p>
-//         </button>
-//         </>
-//     )
-// }
+export const BtnLagre = ({ btnName })=>{
+    return(
+        <>
+        <button className="btnLarge"
+            style={{
+                width: 200,
+                height: 80,
+                backgroundColor:'#070707',
+                color:'#fff',
+                margin:'10px 2px 60px 10px',
+                textAlign: 'center',
+            }}
+        >
+            <p
+                style={{
+                    fontSize: 20,
+                    padding:'25px 0'}}
+            >{btnName}</p>
+        </button>
+        </>
+    )
+}
