461dd6dab942d187f2eeb73d590d6f61bb76e62c
Ducreus <ducanhlaminh@gmail.com>
2022-07-15 20:05:36 +0700

    Update-api

--- my-app/src/api/apiProducts.js
+++ my-app/src/api/apiProducts.js
@@ -1,14 +1,14 @@
 import axiosClients from "./axiosClient";
 
 const ApiProduct = {
-    getAll: (params) => {
-        const url = '/products';
-        return axiosClients.get(url, {params});
-    },
-    get: (id) =>{
-        const url = `/products/${id}`;
-        return axiosClients.get(url);
-    }
-}
+  getAll: (params) => {
+    const url = "/product";
+    return axiosClients.get(url, { params });
+  },
+  get: (id) => {
+    const url = `/products/${id}`;
+    return axiosClients.get(url);
+  },
+};
 
 export default ApiProduct;
--- my-app/src/components/productBoxComponent/productBox.jsx
+++ my-app/src/components/productBoxComponent/productBox.jsx
@@ -1,31 +1,37 @@
-import { Card, Row, Col } from 'antd';
-import React from 'react';
+import { Card, Row, Col } from "antd";
+import React from "react";
 
-function ProductBox() {
-    return (
-        <>
-            <Card
-                hoverable
-                style={{
-                    width: '230px',
-                    height: '324px',
-                    borderRadius: '24px',
-                    margin: '0 4px',
-                    marginBottom : '20px',
-                }}
-                cover={<img alt="example" style={{
-                    width: '230px',
-                    height: '260px',
-                    borderRadius: '24px',
-                }}
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-            >
-                <Row>
-                    <Col></Col>
-                </Row>
-            </Card>
-        </>
-    );
+function ProductBox({ src, index }) {
+  return (
+    <>
+      <Card
+        key={index}
+        hoverable
+        style={{
+          width: "100%",
+          height: "324px",
+          borderRadius: "24px",
+          margin: "0 4px",
+          marginBottom: "20px",
+        }}
+        cover={
+          <img
+            alt="example"
+            style={{
+              width: "100%",
+              height: "260px",
+              borderRadius: "24px",
+            }}
+            src={src}
+          />
+        }
+      >
+        <Row>
+          <Col></Col>
+        </Row>
+      </Card>
+    </>
+  );
 }
 
 export default ProductBox;
